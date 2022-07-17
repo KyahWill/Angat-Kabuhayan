66cc22ff2de878e23b76493e49a4e535d94ef4f3
KyahWill <knownbutnotloved@gmail.com>
2022-07-17 09:51:57 +0800

    Initial Commit

---
+++ nuxt.config.ts
@@ -0,0 +1,16 @@
+import { defineNuxtConfig } from 'nuxt'
+
+// https://v3.nuxtjs.org/api/configuration/nuxt.config
+export default defineNuxtConfig({
+  css: ["@/assets/css/styles.css"],
+  build: {
+    postcss: {
+      postcssOptions:{
+      	plugins: {
+          tailwindcss: {},
+          autoprefixer: {},
+        },
+      },
+    },
+  }
+})
---
+++ pages/index.vue
@@ -0,0 +1,9 @@
+<script>
+
+</script>
+<template>
+	<h5> Hello world </h5>
+
+</template>
+<style>
+</style>
