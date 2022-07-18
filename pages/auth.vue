bbaa22cad6b89342feaa734c46afe67022d03ec6
Kyahwill <knownbutnotloved@gmail.com>
2022-07-18 17:54:23 +0800

    Added the Auth.vue, Added aditional layouts as well

--- pages/auth.vue
+++ pages/auth.vue
@@ -1,9 +1,18 @@
-<script>
+<script setup lang="ts">
+const layout = "landing"
 </script>
 <template>
-	<div>
-		<h1> Hello world </h1>
-	</div>
+	<NuxtLayout :name="layout">
+		<div class="tabs w-50">
+ 			<a class="tab tab-lg tab-lifted">LOG IN</a> 
+			<a class="tab tab-lg tab-lifted tab-active">SIGN UP</a>
+			
+			
+			
+		</div> 
+		<p> WELCOME TO ANGAT-KINABUKASAN  </p>
+
+	</NuxtLayout>
 </template>
 <style>
 </style>
