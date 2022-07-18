551aa9660f20281567e9077eb072f03ab9d7dce0
Kyahwill <knownbutnotloved@gmail.com>
2022-07-18 17:54:59 +0800

    Added the Auth.vue, Added aditional layouts as well

---
+++ layouts/landing.vue
@@ -0,0 +1,12 @@
+<template>
+	<div class="hero min-h-screen bg-base-200">
+		 <div class="hero-content flex-col lg:flex-row">
+	  		 <div class="text-center ">
+	     			 <h1 class="text-5xl font-bold">Login now!</h1>
+	     			 <p class="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
+	   		 </div>
+			<slot />
+	  	 </div>
+
+	</div>
+</template>
