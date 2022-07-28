81c354de0df841c4fdbe673c0c99faca8c7f6d9d
Xavier <hansxavierarambulo@gmail.com>
2022-07-28 11:07:26 +0000

    Update 0 files

---
+++ pages/internshippage1.vue
@@ -0,0 +1,375 @@
+<template>
+<head>
+<link href="https://fonts.googleapis.com/css?family=Montserrat&display=swap" rel="stylesheet" />
+<link href="https://fonts.googleapis.com/css?family=Roboto&display=swap" rel="stylesheet" />
+<link href="https://fonts.googleapis.com/css?family=Inter&display=swap" rel="stylesheet" /></head>
+<body><div class="v77_985"><div class="v77_987"><span class="v77_988">My Profile</span>
+<div class="v77_989">
+<div class="v77_990">
+<div class="v77_991">
+<div class="v77_992">
+</div></div></div></div></div>
+<span class="v77_993">Notification</span>
+<span class="v77_994">Home</span>
+<span class="v77_995">Donate</span>
+<span class="v77_996">Scholarship</span>
+<span class="v77_997">Internship</span>
+<div class="v77_998"><div class="v77_999">
+<span class="v77_1000">Search </span><
+  /div><div class="v77_1001">
+  </div><div class="v77_1002"><div class="v77_1003">
+  <div class="v77_1004"></div></div></div></div>
+  <div class="v77_1005"></div><span class="v77_1006">Rerum reiciendis beatae tenetur excepturi aut pariatur est eos. Sit sit necessitatibus veritatis sed molestiae voluptates incidunt iure sapiente.</span><span class="v77_1007">Rerum reiciendis beatae tenetur excepturi aut pariatur est eos. Sit sit necessitatibus veritatis sed molestiae voluptates incidunt iure sapiente.</span><span class="v77_1008">Rerum reiciendis beatae tenetur excepturi aut pariatur est eos. Sit sit necessitatibus veritatis sed molestiae voluptates incidunt iure sapiente.</span><span class="v77_1009">Rerum reiciendis beatae tenetur excepturi aut pariatur est eos. Sit sit necessitatibus veritatis sed molestiae voluptates incidunt iure sapiente.</span><div class="name"></div><div class="name"></div><div class="v77_1013"></div><div class="name"></div><div class="name"></div><div class="name"></div><div class="v77_1057"></div><span class="v77_2981">INTERNSHIP PROGRAMS</span></div></body><br/><br/> 
+</template>
+<style>
+* {
+  box-sizing: border-box;
+}
+body {
+  font-size: 14px;
+}
+.v77_985 {
+  width: 100%;
+  height: 1063px;
+  background: rgba(246,238,224,1);
+  opacity: 1;
+  position: absolute;
+  top: 0px;
+  left: 0px;
+  overflow: hidden;
+}
+.v77_987 {
+  width: 198px;
+  height: 71px;
+  background: url("../images/v77_987.png");
+  background-repeat: no-repeat;
+  background-position: center center;
+  background-size: cover;
+  opacity: 1;
+  position: absolute;
+  top: 59px;
+  left: 1657px;
+  overflow: hidden;
+}
+.v77_988 {
+  width: 107px;
+  color: rgba(37,43,66,1);
+  position: absolute;
+  top: 17px;
+  left: 90px;
+  font-family: Montserrat;
+  font-weight: SemiBold;
+  font-size: 17px;
+  opacity: 1;
+  text-align: center;
+}
+.v77_989 {
+  width: 68px;
+  height: 71px;
+  background: rgba(232,232,232,1);
+  opacity: 1;
+  position: absolute;
+  top: 0px;
+  left: 0px;
+  border-top-left-radius: 80px;
+  border-top-right-radius: 80px;
+  border-bottom-left-radius: 80px;
+  border-bottom-right-radius: 80px;
+  overflow: hidden;
+}
+.v77_990 {
+  width: 31px;
+  height: 36px;
+  background: rgba(255,255,255,1);
+  opacity: 1;
+  position: absolute;
+  top: 16px;
+  left: 18px;
+  overflow: hidden;
+}
+.v77_991 {
+  width: 29px;
+  height: 34px;
+  background: url("../images/v77_991.png");
+  background-repeat: no-repeat;
+  background-position: center center;
+  background-size: cover;
+  opacity: 1;
+  position: absolute;
+  top: 1px;
+  left: 1px;
+  overflow: hidden;
+}
+.v77_992 {
+  width: 29px;
+  height: 34px;
+  background: url("../images/v77_992.png");
+  opacity: 1;
+  position: absolute;
+  top: 0px;
+  left: 0px;
+  border: 2.447819232940674px solid rgba(37,43,66,1);
+}
+.v77_993 {
+  width: 107px;
+  color: rgba(37,43,66,1);
+  position: absolute;
+  top: 77px;
+  left: 1510px;
+  font-family: Montserrat;
+  font-weight: SemiBold;
+  font-size: 17px;
+  opacity: 1;
+  text-align: center;
+}
+.v77_994 {
+  width: 69px;
+  color: rgba(114,114,114,1);
+  position: absolute;
+  top: 84px;
+  left: 840px;
+  font-family: Montserrat;
+  font-weight: SemiBold;
+  font-size: 20px;
+  opacity: 1;
+  text-align: center;
+}
+.v77_995 {
+  width: 142px;
+  color: rgba(114,114,114,1);
+  position: absolute;
+  top: 84px;
+  left: 1261px;
+  font-family: Montserrat;
+  font-weight: SemiBold;
+  font-size: 20px;
+  opacity: 1;
+  text-align: center;
+}
+.v77_996 {
+  width: 142px;
+  color: rgba(114,114,114,1);
+  position: absolute;
+  top: 84px;
+  left: 941px;
+  font-family: Montserrat;
+  font-weight: SemiBold;
+  font-size: 20px;
+  opacity: 1;
+  text-align: center;
+}
+.v77_997 {
+  width: 142px;
+  color: rgba(114,114,114,1);
+  position: absolute;
+  top: 84px;
+  left: 1103px;
+  font-family: Montserrat;
+  font-weight: SemiBold;
+  font-size: 20px;
+  opacity: 1;
+  text-align: center;
+}
+.v77_998 {
+  width: 600px;
+  height: 40px;
+  background: url("../images/v77_998.png");
+  background-repeat: no-repeat;
+  background-position: center center;
+  background-size: cover;
+  opacity: 1;
+  position: absolute;
+  top: 74px;
+  left: 224px;
+  overflow: hidden;
+}
+.v77_999 {
+  width: 531px;
+  height: 40px;
+  background: rgba(245,245,245,1);
+  padding: 21px 28px;
+  margin: 54px;
+  opacity: 1;
+  position: absolute;
+  top: 0px;
+  left: 0px;
+  border-top-left-radius: 5px;
+  border-bottom-left-radius: 5px;
+  overflow: hidden;
+}
+.v77_1000 {
+  width: 56px;
+  color: rgba(114,114,114,1);
+  position: absolute;
+  top: 3px;
+  left: 30px;
+  font-family: Montserrat;
+  font-weight: Regular;
+  font-size: 16px;
+  opacity: 1;
+  text-align: left;
+}
+.v77_1001 {
+  width: 179px;
+  height: 40px;
+  background: url("../images/v77_1001.png");
+  background-repeat: no-repeat;
+  background-position: center center;
+  background-size: cover;
+  opacity: 1;
+  position: absolute;
+  top: 0px;
+  left: 352px;
+  overflow: hidden;
+}
+.v77_1002 {
+  width: 69px;
+  height: 40px;
+  background: url("../images/v77_1002.png");
+  background-repeat: no-repeat;
+  background-position: center center;
+  background-size: cover;
+  opacity: 1;
+  position: absolute;
+  top: 1px;
+  left: 530px;
+  overflow: hidden;
+}
+.v77_1003 {
+  width: 69px;
+  height: 40px;
+  background: rgba(164,92,64,1);
+  padding: 21px 23px;
+  margin: 11px;
+  opacity: 1;
+  position: absolute;
+  top: 0px;
+  left: 0px;
+  border-top-right-radius: 5px;
+  border-bottom-right-radius: 5px;
+  overflow: hidden;
+}
+.v77_1004 {
+  width: 23px;
+  height: 23px;
+  background: rgba(255,255,255,1);
+  opacity: 1;
+  position: absolute;
+  top: 7px;
+  left: 23px;
+}
+.v77_1005 {
+  width: 285px;
+  height: 299px;
+  background: url("../images/v77_1005.png");
+  background-repeat: no-repeat;
+  background-position: center center;
+  background-size: cover;
+  opacity: 1;
+  position: absolute;
+  top: 39px;
+  left: 16px;
+  overflow: hidden;
+}
+.v77_1006 {
+  width: 210px;
+  color: rgba(31,41,55,1);
+  position: absolute;
+  top: 1076px;
+  left: 242px;
+  font-family: Roboto;
+  font-weight: Regular;
+  font-size: 16px;
+  opacity: 1;
+  text-align: left;
+}
+.v77_1007 {
+  width: 210px;
+  color: rgba(31,41,55,1);
+  position: absolute;
+  top: 1073px;
+  left: 1343px;
+  font-family: Roboto;
+  font-weight: Regular;
+  font-size: 16px;
+  opacity: 1;
+  text-align: left;
+}
+.v77_1008 {
+  width: 210px;
+  color: rgba(31,41,55,1);
+  position: absolute;
+  top: 1073px;
+  left: 976px;
+  font-family: Roboto;
+  font-weight: Regular;
+  font-size: 16px;
+  opacity: 1;
+  text-align: left;
+}
+.v77_1009 {
+  width: 210px;
+  color: rgba(31,41,55,1);
+  position: absolute;
+  top: 1076px;
+  left: 609px;
+  font-family: Roboto;
+  font-weight: Regular;
+  font-size: 16px;
+  opacity: 1;
+  text-align: left;
+}
+.name {
+  color: #fff;
+}
+.name {
+  color: #fff;
+}
+.v77_1013 {
+  width: 52px;
+  height: 58px;
+  background: url("../images/v77_1013.png");
+  background-repeat: no-repeat;
+  background-position: center center;
+  background-size: cover;
+  opacity: 1;
+  position: absolute;
+  top: 71px;
+  left: 1450px;
+  overflow: hidden;
+}
+.name {
+  color: #fff;
+}
+.name {
+  color: #fff;
+}
+.name {
+  color: #fff;
+}
+.v77_1057 {
+  width: 390px;
+  height: 793px;
+  background: url("../images/v77_1057.png");
+  background-repeat: no-repeat;
+  background-position: center center;
+  background-size: cover;
+  opacity: 1;
+  position: absolute;
+  top: 270px;
+  left: 1476px;
+  overflow: hidden;
+}
+.v77_2981 {
+  width: 608px;
+  color: rgba(71,83,102,1);
+  position: absolute;
+  top: 230px;
+  left: 742px;
+  font-family: Inter;
+  font-weight: Extra Bold;
+  font-size: 50px;
+  opacity: 1;
+  text-align: left;
+}
+</style>
