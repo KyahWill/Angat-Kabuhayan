624417151f7cba809bc29a165f90526038a123f3
Kyahwill <knownbutnotloved@gmail.com>
2022-07-31 23:15:47 +0800

    improved the view of the home page

--- pages/home/index.vue
+++ pages/home/index.vue
@@ -1,123 +1,618 @@
+<script setup lang="ts">
+// https://x9t4gr.sse.codesandbox.io/landingpage
+</script>
+
+
 <template>
   <div name="root">
     <Head>
-      <link
+      <Link
         href="https://fonts.googleapis.com/css?family=Montserrat&display=swap"
         rel="stylesheet"
       />
-      <link
-        href="https://fonts.googleapis.com/css?family=Inter&display=swap"
-        rel="stylesheet"
-      />
-      <link
+      <Link
         href="https://fonts.googleapis.com/css?family=Roboto&display=swap"
         rel="stylesheet"
       />
-      <link href="./css/main.css" rel="stylesheet" /><title>Document</title>
+      <Link
+        href="https://fonts.googleapis.com/css?family=DM+Sans&display=swap"
+        rel="stylesheet"
+      />
+      <Link
+        href="https://fonts.googleapis.com/css?family=DM+Serif+Display&display=swap"
+        rel="stylesheet"
+      />
+      <Link href="./css/main.css" rel="stylesheet" />
+      <Title>Student View</Title>
     </Head>
-    <NuxtLayout name="auth">
-      <span class="v77_2982">SCHOLARSHIP PROGRAMS</span>
-    </NuxtLayout>
-    <div class="v160_1900">
-      <div class="v162_1491"></div>
-      <div class="v162_1490"></div>
-      <span class="v160_1901">(STUDENT)</span>
-      <span class="v160_1902">21 YEARS OLD</span>
-      <span class="v160_1903">QUEZON CITY, PHILIPPINES</span>
-      <span class="v160_1904">PRIVATE UNIVERSITY U BELT</span>
-      <span class="v160_1905">LOREMIPSUM@GMAIL.COM</span>
-      <span class="v160_1906">ABOUT MYSELF</span>
-      <div class="v160_2178"></div>
-      <div class="v160_2179"></div>
-      <span class="v160_1908"
-        >Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-        commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
-        velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
-        occaecat cupidatat non proident, sunt in culpa qui officia deserunt
-        mollit anim id est laborum.</span
-      >
-      <div class="v160_2175"></div>
-      <span class="v160_2177"
-        >Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-        commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
-        velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
-        occaecat cupidatat non proident, sunt in culpa qui officia deserunt
-        mollit anim id est laborum.</span
-      >
-      <span class="v160_1909">EDUCATION</span>
-      <span class="v160_1910"
-        >Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-        commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
-        velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
-        occaecat cupidatat non proident, sunt in culpa qui officia deserunt
-        mollit anim id est laborum.</span
-      >
-      <span class="v160_1911">AWARD AND HONORS</span>
-      <span class="v160_1912">Rose Ann Fuentes</span>
-      <div class="v160_1913"></div>
-      <div class="v160_1914">
-        <div class="v160_1915">
-          <span class="v160_1916">Search </span>
-        </div>
-        <div class="v160_1917"></div>
-        <div class="v160_1918">
-          <div class="v160_1919">
-            <div class="v160_1920"></div>
-          </div>
-        </div>
-      </div>
-      <div class="v160_1921"></div>
-      <div class="name"></div>
-      <div class="name"></div>
-      <div class="v160_2180"></div>
-      <div class="name"></div>
-      <div class="v160_1926"></div>
-      <span class="v160_1927">Notification</span>
-      <div class="v160_1928"></div>
-      <div class="v160_1931">
-        <span class="v160_1932">Prototyping</span>
-        <span class="v160_1933">Wireframing</span>
-        <span class="v160_1934">User Flow</span>
-        <span class="v160_1935">User Research</span>
-        <span class="v160_1936">User interviews</span>
-      </div>
-      <div class="v162_1489"></div>
-      <span class="v160_1937">skills</span>
-      <div class="v160_1938">
-        <span class="v160_1939">Prototyping</span>
-        <span class="v160_1940">Wireframing</span>
-        <span class="v160_1941">User Flow</span>
-        <span class="v160_1942">User Research</span>
-        <span class="v160_1943">User interviews</span>
-      </div>
-      <span class="v160_1944">skills</span>
-      <div class="v160_1945">
-        <span class="v160_1946">My Profile</span>
-        <div class="v160_1947">
-          <div class="v160_1948">
-            <div class="v160_1949">
-              <div class="v160_1950"></div>
+
+    <div class="v77_1238">
+      <TheHeadernew />
+      <NuxtLayout name="auth">
+
+      <div class="v77_1243">
+        <span class="v77_1244">My Profile</span>
+        <div class="v77_1245">
+          <div class="v77_1246">
+            <div class="v77_1247">
+              <div class="v77_1248"></div>
             </div>
           </div>
         </div>
+        <span class="v77_1249">Notification</span>
+        <div class="v77_1254">
+          <div class="v77_1255">
+            <span class="v77_1256">Search </span>
+          </div>
+          <div class="v77_1257"></div>
+          <div class="v77_1258">
+            <div class="v77_1259">
+              <div class="v77_1260"></div>
+            </div>
+          </div>
+        </div>
+        <div class="v77_1261"></div>
+        <span class="v77_1262"
+          >Rerum reiciendis beatae tenetur excepturi aut pariatur est eos. Sit
+          sit necessitatibus veritatis sed molestiae voluptates incidunt iure
+          sapiente.</span
+        >
+        <div class="name"></div>
+        <div class="v77_1268"></div>
+        <span class="v77_3233">APPLICATIONS</span>
+        <span class="v77_3234">Internship Program</span>
+        <div class="v77_3197"></div>
+        <div class="v77_3200"></div>
+        <span class="v77_3201">WELCOME, JANE!</span>
+        <div class="name"></div>
+        <span class="v77_3204">Lagro High School INC</span>
+        <span class="v79_1210">STUDENT</span>
+        <span class="v77_3213"
+          >Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+          culpa qui officia deserunt mollit anim id est laborum.</span
+        >
+        <div class="name"></div>
+        <span class="v77_3205">JANE SMITH</span>
+        <span class="v77_3212">About Myself</span>
+        <span class="v153_1578">Donation Program</span>
+        <div class="v159_1369">
+          <div class="v159_1370"></div>
+          <div class="v159_1371"></div>
+          <div class="v159_1374"></div>
+          <span class="v159_1375">VIEW</span>
+          <span class="v159_1372">PUP LABORATORY UNIFORM</span>
+          <span class="v159_1376"
+            >Size: Medium (Female) Meet up place: Sta. Mesa Jollibee Contact:
+            09675411323</span
+          >
+        </div>
+        <span class="v160_1511">Home</span>
+        <span class="v160_1512">Donate</span>
+        <span class="v160_1513">Scholarship</span>
+        <span class="v160_1514">Internship</span>
+        <div class="name"></div>
+        <span class="v160_1891">Scholarship Program</span>
+        <div class="name"></div>
       </div>
-      <span class="v160_1951">Home</span>
-      <span class="v160_1952">Donate</span>
-      <span class="v160_1953">Scholarship</span>
-      <span class="v160_1954">Internship</span>
-      <div class="v161_1623">
-        <div class="v161_1624"></div>
-        <span class="v161_1625">SAVE</span>
-      </div>
+      </NuxtLayout>
     </div>
   </div>
 </template>
-
+  
 <style>
+* {
+  box-sizing: border-box;
+}
+body {
+  font-size: 14px;
+}
+.v77_1238 {
+  width: 100%;
+  height: 1080px;
+  background: rgba(246, 238, 224, 1);
+  opacity: 1;
+  position: relative;
+  top: 0px;
+  left: 0px;
+  overflow: hidden;
+}
+.v77_1243 {
+  width: 198px;
+  height: 71px;
+  background: url("../images/v77_1243.png");
+  background-repeat: no-repeat;
+  background-position: center center;
+  background-size: cover;
+  opacity: 1;
+  position: absolute;
+  top: 67px;
+  left: 1649px;
+  overflow: hidden;
+}
+.v77_1244 {
+  width: 107px;
+  color: rgba(37, 43, 66, 1);
+  position: absolute;
+  top: 17px;
+  left: 91px;
+  font-family: Montserrat;
+  font-weight: SemiBold;
+  font-size: 17px;
+  opacity: 1;
+  text-align: center;
+}
+.v77_1245 {
+  width: 68px;
+  height: 71px;
+  background: rgba(232, 232, 232, 1);
+  opacity: 1;
+  position: relative;
+  top: 0px;
+  left: 0px;
+  border-top-left-radius: 80px;
+  border-top-right-radius: 80px;
+  border-bottom-left-radius: 80px;
+  border-bottom-right-radius: 80px;
+  overflow: hidden;
+}
+.v77_1246 {
+  width: 31px;
+  height: 36px;
+  background: rgba(255, 255, 255, 1);
+  opacity: 1;
+  position: absolute;
+  top: 16px;
+  left: 19px;
+  overflow: hidden;
+}
+.v77_1247 {
+  width: 29px;
+  height: 34px;
+  background: url("../images/v77_1247.png");
+  background-repeat: no-repeat;
+  background-position: center center;
+  background-size: cover;
+  opacity: 1;
+  position: absolute;
+  top: 1px;
+  left: 2px;
+  overflow: hidden;
+}
+.v77_1248 {
+  width: 29px;
+  height: 34px;
+  background: url("../images/v77_1248.png");
+  opacity: 1;
+  position: relative;
+  top: 0px;
+  left: 0px;
+  border: 2.447819232940674px solid rgba(37, 43, 66, 1);
+}
+.v77_1249 {
+  width: 107px;
+  color: rgba(37, 43, 66, 1);
+  position: absolute;
+  top: 85px;
+  left: 1502px;
+  font-family: Montserrat;
+  font-weight: SemiBold;
+  font-size: 17px;
+  opacity: 1;
+  text-align: center;
+}
+.v77_1254 {
+  width: 584px;
+  height: 57px;
+  background: url("../images/v77_1254.png");
+  background-repeat: no-repeat;
+  background-position: center center;
+  background-size: cover;
+  opacity: 1;
+  position: absolute;
+  top: 74px;
+  left: 224px;
+  overflow: hidden;
+}
+.v77_1255 {
+  width: 515px;
+  height: 57px;
+  background: rgba(245, 245, 245, 1);
+  padding: 21px 28px;
+  margin: 54px;
+  opacity: 1;
+  position: relative;
+  top: 0px;
+  left: 0px;
+  border-top-left-radius: 5px;
+  border-bottom-left-radius: 5px;
+  overflow: hidden;
+}
+.v77_1256 {
+  width: 56px;
+  color: rgba(114, 114, 114, 1);
+  position: absolute;
+  top: 12px;
+  left: 31px;
+  font-family: Montserrat;
+  font-weight: Regular;
+  font-size: 16px;
+  opacity: 1;
+  text-align: left;
+}
+.v77_1257 {
+  width: 179px;
+  height: 57px;
+  background: url("../images/v77_1257.png");
+  background-repeat: no-repeat;
+  background-position: center center;
+  background-size: cover;
+  opacity: 1;
+  position: absolute;
+  top: 0px;
+  left: 337px;
+  overflow: hidden;
+}
+.v77_1258 {
+  width: 69px;
+  height: 57px;
+  background: url("../images/v77_1258.png");
+  background-repeat: no-repeat;
+  background-position: center center;
+  background-size: cover;
+  opacity: 1;
+  position: absolute;
+  top: 1px;
+  left: 515px;
+  overflow: hidden;
+}
+.v77_1259 {
+  width: 69px;
+  height: 57px;
+  background: rgba(164, 92, 64, 1);
+  padding: 21px 23px;
+  margin: 11px;
+  opacity: 1;
+  position: relative;
+  top: 0px;
+  left: 0px;
+  border-top-right-radius: 5px;
+  border-bottom-right-radius: 5px;
+  overflow: hidden;
+}
+.v77_1260 {
+  width: 23px;
+  height: 23px;
+  background: rgba(255, 255, 255, 1);
+  opacity: 1;
+  position: absolute;
+  top: 16px;
+  left: 24px;
+}
+.v77_1261 {
+  width: 285px;
+  height: 299px;
+  background: url("../images/v77_1261.png");
+  background-repeat: no-repeat;
+  background-position: center center;
+  background-size: cover;
+  opacity: 1;
+  position: absolute;
+  top: 39px;
+  left: 16px;
+  overflow: hidden;
+}
+.v77_1262 {
+  width: 209px;
+  color: rgba(31, 41, 55, 1);
+  position: absolute;
+  top: 1094px;
+  left: 240px;
+  font-family: Roboto;
+  font-weight: Regular;
+  font-size: 16px;
+  opacity: 1;
+  text-align: left;
+}
+.name {
+  color: #fff;
+}
+.v77_1268 {
+  width: 52px;
+  height: 58px;
+  background: url("../images/v77_1268.png");
+  background-repeat: no-repeat;
+  background-position: center center;
+  background-size: cover;
+  opacity: 1;
+  position: absolute;
+  top: 71px;
+  left: 1450px;
+  overflow: hidden;
+}
+.v77_3233 {
+  width: 358px;
+  color: rgba(0, 0, 0, 1);
+  position: absolute;
+  top: 243px;
+  left: 666px;
+  font-family: DM Sans;
+  font-weight: Medium;
+  font-size: 40px;
+  opacity: 1;
+  text-align: left;
+}
+.v77_3234 {
+  width: 343px;
+  color: rgba(0, 0, 0, 1);
+  position: absolute;
+  top: 321px;
+  left: 655px;
+  font-family: DM Sans;
+  font-weight: Medium;
+  font-size: 30px;
+  opacity: 1;
+  text-align: left;
+}
+.v77_3197 {
+  width: 610px;
+  height: 820px;
+  background: rgba(164, 92, 64, 1);
+  opacity: 1;
+  position: absolute;
+  top: 245px;
+  left: 6px;
+  overflow: hidden;
+}
+.v77_3200 {
+  width: 610px;
+  height: 1px;
+  background: rgba(215, 215, 215, 1);
+  opacity: 1;
+  position: absolute;
+  top: 139px;
+  left: 106px;
+  overflow: hidden;
+}
+.v77_3201 {
+  width: 539px;
+  color: rgba(255, 255, 255, 1);
+  position: absolute;
+  top: 292px;
+  left: 44px;
+  font-family: DM Sans;
+  font-weight: Bold;
+  font-size: 60px;
+  opacity: 1;
+  text-align: left;
+}
+.name {
+  color: #fff;
+}
+.v77_3204 {
+  width: 209px;
+  color: rgba(255, 255, 255, 1);
+  position: absolute;
+  top: 731px;
+  left: 201px;
+  font-family: DM Serif Display;
+  font-weight: Regular;
+  font-size: 20px;
+  opacity: 0.4000000059604645;
+  text-align: left;
+}
+.v79_1210 {
+  width: 209px;
+  color: rgba(255, 255, 255, 1);
+  position: absolute;
+  top: 759px;
+  left: 261px;
+  font-family: DM Serif Display;
+  font-weight: Regular;
+  font-size: 20px;
+  opacity: 0.4000000059604645;
+  text-align: left;
+}
+.v77_3213 {
+  width: 434px;
+  color: rgba(255, 255, 255, 1);
+  position: absolute;
+  top: 872px;
+  left: 98px;
+  font-family: DM Sans;
+  font-weight: Regular;
+  font-size: 14px;
+  opacity: 0.4000000059604645;
+  text-align: left;
+}
+.name {
+  color: #fff;
+}
+.v77_3205 {
+  width: 233px;
+  color: rgba(255, 255, 255, 1);
+  position: absolute;
+  top: 678px;
+  left: 190px;
+  font-family: DM Serif Display;
+  font-weight: Regular;
+  font-size: 40px;
+  opacity: 1;
+  text-align: left;
+}
+.v77_3212 {
+  width: 202px;
+  color: rgba(255, 255, 255, 1);
+  position: absolute;
+  top: 821px;
+  left: 97px;
+  font-family: DM Sans;
+  font-weight: Medium;
+  font-size: 22px;
+  opacity: 1;
+  text-align: left;
+}
+.v153_1578 {
+  width: 396px;
+  color: rgba(0, 0, 0, 1);
+  position: absolute;
+  top: 657px;
+  left: 653px;
+  font-family: DM Sans;
+  font-weight: Medium;
+  font-size: 30px;
+  opacity: 1;
+  text-align: left;
+}
+.v159_1369 {
+  width: 100%;
+  height: 328px;
+  background: url("../images/v159_1369.png");
+  background-repeat: no-repeat;
+  background-position: center center;
+  background-size: cover;
+  opacity: 1;
+  position: absolute;
+  top: 720px;
+  left: 740px;
+  overflow: hidden;
+}
+.v159_1370 {
+  width: 100%;
+  height: 328px;
+  background: rgba(195, 131, 112, 1);
+  opacity: 1;
+  position: absolute;
+  top: 0px;
+  left: 0px;
+  overflow: hidden;
+}
+.v159_1371 {
+  width: 298px;
+  height: 285px;
+  background: url("../images/v159_1371.png");
+  background-repeat: no-repeat;
+  background-position: center center;
+  background-size: cover;
+  opacity: 1;
+  position: absolute;
+  top: 23px;
+  left: 37px;
+  overflow: hidden;
+}
+.v159_1374 {
+  width: 205px;
+  height: 43px;
+  background: rgba(255, 255, 255, 1);
+  opacity: 1;
+  position: absolute;
+  top: 236px;
+  left: 773px;
+}
+.v159_1375 {
+  width: 86px;
+  color: rgba(0, 0, 0, 1);
+  position: absolute;
+  top: 249px;
+  left: 827px;
+  font-family: Montserrat;
+  font-weight: Light;
+  font-size: 20px;
+  opacity: 1;
+  text-align: center;
+}
+.v159_1372 {
+  width: 480px;
+  color: rgba(255, 255, 255, 1);
+  position: absolute;
+  top: 61px;
+  left: 395px;
+  font-family: Montserrat;
+  font-weight: Bold;
+  font-size: 30px;
+  opacity: 1;
+  text-align: center;
+}
+.v159_1376 {
+  width: 335px;
+  color: rgba(255, 255, 255, 1);
+  position: absolute;
+  top: 123px;
+  left: 409px;
+  font-family: Montserrat;
+  font-weight: Light;
+  font-size: 20px;
+  opacity: 1;
+  text-align: left;
+}
+.v160_1511 {
+  width: 69px;
+  color: rgba(114, 114, 114, 1);
+  position: absolute;
+  top: 85px;
+  left: 854px;
+  font-family: Montserrat;
+  font-weight: SemiBold;
+  font-size: 20px;
+  opacity: 1;
+  text-align: center;
+}
+.v160_1512 {
+  width: 142px;
+  color: rgba(114, 114, 114, 1);
+  position: absolute;
+  top: 85px;
+  left: 1275px;
+  font-family: Montserrat;
+  font-weight: SemiBold;
+  font-size: 20px;
+  opacity: 1;
+  text-align: center;
+}
+.v160_1513 {
+  width: 142px;
+  color: rgba(114, 114, 114, 1);
+  position: absolute;
+  top: 85px;
+  left: 955px;
+  font-family: Montserrat;
+  font-weight: SemiBold;
+  font-size: 20px;
+  opacity: 1;
+  text-align: center;
+}
+.v160_1514 {
+  width: 142px;
+  color: rgba(114, 114, 114, 1);
+  position: absolute;
+  top: 85px;
+  left: 1117px;
+  font-family: Montserrat;
+  font-weight: SemiBold;
+  font-size: 20px;
+  opacity: 1;
+  text-align: center;
+}
+.name {
+  color: #fff;
+}
+.v160_1891 {
+  width: 343px;
+  color: rgba(0, 0, 0, 1);
+  position: absolute;
+  top: 321px;
+  left: 1284px;
+  font-family: DM Sans;
+  font-weight: Medium;
+  font-size: 30px;
+  opacity: 1;
+  text-align: left;
+}
+.name {
+  color: #fff;
+}
 </style>
